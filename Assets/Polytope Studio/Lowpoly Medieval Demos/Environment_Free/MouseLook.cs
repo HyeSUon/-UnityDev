1b0ed5fc9381c18270e13c8656e6d2ba45f12303
HyeonSu <hyeonsu@kpu.ac.kr>
2022-11-03 10:59:50 +0900

    Move 스크립트 수정

--- Assets/Polytope Studio/Lowpoly Medieval Demos/Environment_Free/MouseLook.cs
+++ Assets/Polytope Studio/Lowpoly Medieval Demos/Environment_Free/MouseLook.cs
@@ -18,7 +18,7 @@ public class MouseLook : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X") * mouseS 
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * 0.1f;
         transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
         playerBody.Rotate(Vector3.up * mouseX);
     }
